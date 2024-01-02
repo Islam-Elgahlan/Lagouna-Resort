e93bc09ef70e8c5f8f2158f60f096062d3d18c76
Islam-Elgahlan <islam.elgahlan@gmail.com>
2024-01-02 16:40:08 +0200

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
+HttpClientModule
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      closeButton	: true ,
+      timeOut: 2000
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+
+  onLogin(data:any):Observable<any>{
+    return this._HttpClient.post('admin/users/login' , data)
+  }
 }
