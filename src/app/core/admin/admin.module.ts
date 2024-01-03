d5997e457832911cecd2072bbf9007060e668672
Salhoom75 <salhoomadel@gmail.com>
2024-01-03 19:50:25 +0200

    initially

--- src/app/core/admin/admin.module.ts
+++ src/app/core/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
