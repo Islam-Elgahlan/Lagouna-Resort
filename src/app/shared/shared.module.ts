8fe5e3b32e471d8eaaea28926da79838a0f0c970
Salhoom75 <salhoomadel@gmail.com>
2024-01-03 18:27:30 +0200

    reset ui+integrstion

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import {MatIconModule} from '@angular/material/icon';
 
-
-
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -13,9 +13,11 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    NgxDropzoneModule
+    NgxDropzoneModule,
+    MatIconModule,
+    MatButtonModule
     
   ],
-  exports: [ReactiveFormsModule,FormsModule,NgxDropzoneModule]
+  exports: [ReactiveFormsModule,FormsModule,NgxDropzoneModule,MatIconModule,MatButtonModule]
 })
 export class SharedModule { }
