733648a417884d7bc8cbffacc2a4cfd41344d199
EmanMohamed22 <emyelgarf22@gmail.com>
2024-01-04 00:24:03 +0200

    edit arch

--- src/app/core/admin/admin-routing.module.ts
+++ src/app/core/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
+
 
 const routes: Routes = [
-  {path:'dashboard',component:DashboardComponent}
+  {path:'',redirectTo:'',pathMatch:'full'},
+   {path:'dashboard',loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)}
 ];
 
 @NgModule({
