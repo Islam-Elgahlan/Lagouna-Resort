fdf2d988b18d857b2d3982c543d00b676a594c45
Salhoom75 <salhoomadel@gmail.com>
2024-01-04 00:57:19 +0200

    .

--- src/app/core/admin/home/home.module.ts
+++ src/app/core/admin/home/home.module.ts
@@ -3,13 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { Routes } from '@angular/router';
 import { RoomsComponent } from '../rooms/rooms.component';
-const routes:Routes=[
-  {path:'',component:HomeComponent}
-]
+const routes: Routes = [{ path: '', component: HomeComponent }];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HomeComponent]
+  imports: [CommonModule], 
+  declarations: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
