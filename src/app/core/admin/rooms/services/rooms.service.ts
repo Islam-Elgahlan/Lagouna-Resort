3baf6dafb081eeede02a0e1adc4a29d4d5826103
Salhoom75 <salhoomadel@gmail.com>
2024-01-06 00:17:39 +0200

    start

--- src/app/core/admin/rooms/services/rooms.service.ts
+++ src/app/core/admin/rooms/services/rooms.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoomsService {
+  constructor(private _HttpClient: HttpClient) {}
 
-  constructor(private _HttpClient:HttpClient) { }
-  
+  onGetRooms(data: any): Observable<any> {
+    return this._HttpClient.get('admin/rooms',{params:data})
+  }
 }
