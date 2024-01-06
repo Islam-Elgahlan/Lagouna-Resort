97950c47283dc7b5cbfaf8912a271870547e1cc2
EmanMohamed22 <emyelgarf22@gmail.com>
2024-01-06 16:31:41 +0200

    list

--- src/app/core/admin/facilities/components/facilities-home/facilities-home.component.ts
+++ src/app/core/admin/facilities/components/facilities-home/facilities-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FacilitiesService } from '../../services/facilities.service';
 
 @Component({
   selector: 'app-facilities-home',
   templateUrl: './facilities-home.component.html',
   styleUrls: ['./facilities-home.component.scss']
 })
-export class FacilitiesHomeComponent {
+export class FacilitiesHomeComponent implements OnInit{
 tableData:any;
+constructor(private _Facilities:FacilitiesService,){}
+  ngOnInit(): void {
+    this.getAllFacilities()
+  }
+ 
+getAllFacilities(){
+  this._Facilities.getAllFacilities().subscribe({
+    next:(res)=>{
+      console.log(res.data.facilities);
+      this.tableData=res.data.facilities
+    },error:(err)=>{
+
+    },complete:()=> {
+      
+    },
+  })
+}
+
+
 }
