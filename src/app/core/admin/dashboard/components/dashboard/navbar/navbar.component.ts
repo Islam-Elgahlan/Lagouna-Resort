eec62175d151c136fffddfb323afbe222ea84102
Salhoom75 <salhoomadel@gmail.com>
2024-01-07 19:25:58 +0200

    test1

--- src/app/core/admin/dashboard/components/dashboard/navbar/navbar.component.ts
+++ src/app/core/admin/dashboard/components/dashboard/navbar/navbar.component.ts
@@ -1,32 +1,36 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ChangePasswordComponent } from 'src/app/core/auth/components/chabge-password/change-password.component';
 import { LogoutComponent } from 'src/app/shared/logout/logout.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  userName:string=''
-constructor(private _MatDialog:MatDialog){
-  this.getUserName()
-}
+  userName: string = '';
+  constructor(private dialog: MatDialog) {
+    this.getUserName();
+  }
 
-getUserName(){
-this.userName=localStorage.getItem('userName')!
-}
+  getUserName() {
+    this.userName = localStorage.getItem('userName')!;
+  }
 
-openLogOutDialogue(){
-  const dialogRef = this._MatDialog.open(LogoutComponent, {
-    
-    width: '30%',
-  });
-  dialogRef.afterClosed().subscribe((result) => {
-    console.log('The dialog was closed');
-    
-  });
-}
-}
+  openLogOutDialogue() {
+    const dialogRef = this.dialog.open(LogoutComponent, {
+      width: '30%',
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 
-
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ChangePasswordComponent, {
+      data: {},
+      width: '40%',
+    });
+  }
+}
