2ab93a4e2256532b9fceb5eff9298bc8ee0fdc7c
EmanMohamed22 <emyelgarf22@gmail.com>
2024-01-17 18:29:31 +0200

    translate

--- src/app/core/landing-page/home/components/gallery/gallery.component.ts
+++ src/app/core/landing-page/home/components/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { HelperService } from 'src/app/shared/services/helper.service';
 
 @Component({
   selector: 'app-gallery',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
-
+  constructor(public _HelperService:HelperService , public translate: TranslateService){
+    
+  }
+  onChangeLang(lang:string){
+    this._HelperService.onChangeLang(lang)
+    // console.log(this.translate.currentLang);
+    
+  }
 }
