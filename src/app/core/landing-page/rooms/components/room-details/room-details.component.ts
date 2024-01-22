67444304249c74e4d99ea6bbd97032a19d301144
Salhoom75 <salhoomadel@gmail.com>
2024-01-22 02:43:19 +0200

    .

--- src/app/core/landing-page/rooms/components/room-details/room-details.component.ts
+++ src/app/core/landing-page/rooms/components/room-details/room-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { RoomService } from '../../services/room.service';
 import { HelperService } from 'src/app/shared/services/helper.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-room-details',
@@ -13,13 +14,14 @@ export class RoomDetailsComponent {
   roomData: any;
   roomId: string = '';
   imgs: [] = [];
-  counter:number=1;
-  hideRequiredMarker:boolean=true;
+  counter: number = 1;
+  hideRequiredMarker: boolean = true;
   constructor(
     private _ActivatedRoute: ActivatedRoute,
     private _RoomService: RoomService,
-    private _HelperService:HelperService,
-    private _Router:Router
+    private _HelperService: HelperService,
+    private _Router: Router,
+    private _TranslateService: TranslateService
   ) {
     this.roomId = _ActivatedRoute.snapshot.params['id'];
     console.log(this.roomId);
@@ -43,34 +45,25 @@ export class RoomDetailsComponent {
   }
 
   rangeForm = new FormGroup({
-    start: new FormControl<Date | null>(null,[Validators.required]),
-    end: new FormControl<Date | null>(null,[Validators.required]),
-     capacity:new FormControl(null),
-    
+    start: new FormControl<Date | null>(null, [Validators.required]),
+    end: new FormControl<Date | null>(null, [Validators.required]),
+    capacity: new FormControl(null),
   });
-  onChangeLang(lang:string){
-    this._HelperService.onChangeLang(lang)
+  onChangeLang(lang: string) {
+    this._HelperService.onChangeLang(lang);
     // console.log(this.translate.currentLang);
-    
   }
-  createBooking(){
-
+  createBooking() {}
+  onIncrement() {
+    this.counter++;
   }
-  onIncrement(){
-    
-  this.counter++
-
-
-  }
-  onDecrement(){
-    if (this.counter>=1) {
-      this.counter--
+  onDecrement() {
+    if (this.counter >= 1) {
+      this.counter--;
     }
-    
   }
-  onSubmit(form:FormGroup){
+  onSubmit(form: FormGroup) {
     console.log(form.value);
-    this._Router.navigate(['/landingPage/rooms/allRooms'])
-      }
-      
+    this._Router.navigate(['/landingPage/rooms/allRooms']);
+  }
 }
