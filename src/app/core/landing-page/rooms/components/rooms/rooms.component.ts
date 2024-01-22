d4ba3eb0f75dee62528d7e576debf5a62ecdd2da
Salhoom75 <salhoomadel@gmail.com>
2024-01-22 03:20:59 +0200

    ..

--- src/app/core/landing-page/rooms/components/rooms/rooms.component.ts
+++ src/app/core/landing-page/rooms/components/rooms/rooms.component.ts
@@ -2,41 +2,38 @@ import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { RoomService } from '../../services/room.service';
 import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { HelperService } from 'src/app/shared/services/helper.service';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
-  
   pageSize: number = 100;
   pageNumber: number | undefined = 1;
   tableResponse: any;
   tableData: any[] = [];
-  routingTitle1:string='';
-  routingTitle2:string='';
+  routingTitle1: string = '';
+  routingTitle2: string = '';
   constructor(
     private _toastr: ToastrService,
     private _RoomService: RoomService,
-    private _TitleService: Title
-  ) { }
-  
-  
+    private _TitleService: Title,
+    private _TranslateService: TranslateService,
+    private _HelperService: HelperService
+  ) {}
 
-  
   ngOnInit() {
     this.onGetAllRooms();
-    this.getTitle()
+    this.getTitle();
   }
   getTitle() {
-    
-   
-    this.routingTitle1=this._TitleService.getTitle();
-    this.routingTitle2=this.routingTitle1.substring(11,);
+    this.routingTitle1 = this._TitleService.getTitle();
+    this.routingTitle2 = this.routingTitle1.substring(11);
     console.log(this.routingTitle2);
-    
-}
+  }
   onGetAllRooms() {
     let x = {
       size: this.pageSize,
@@ -49,12 +46,14 @@ export class RoomsComponent {
         this.tableResponse = res.data;
         this.tableData = res.data.rooms;
         console.log(this.tableData);
-
       },
       error: (err) => {
         console.log(err);
       },
     });
-
+  }
+  onChangeLang(lang: string) {
+    this._HelperService.onChangeLang(lang);
+    // console.log(this.translate.currentLang);
   }
 }
