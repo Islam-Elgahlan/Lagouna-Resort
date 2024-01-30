17aa278267afedc46711c67c3ba72dfdf224e5ab
EmanMohamed22 <emyelgarf22@gmail.com>
2024-01-30 22:04:01 +0200

    dash

--- src/app/core/admin/dashboard/components/home/home.component.ts
+++ src/app/core/admin/dashboard/components/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddAdsComponent } from '../../../ads/components/add-ads/add-ads.component';
+import { DashService } from '../../services/dash.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+constructor(private _DashService:DashService){}
+ngOnInit(): void {
+  this.getAllData()
+}
+getAllData(){
+  this._DashService.getData().subscribe({
+    next:(res)=>{
+      console.log(res);
+      
+    }
+  })
+}
 }
